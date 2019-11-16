01dff32c05a89fbc2d0d336b4e8735e9e8a7e607
Tuan Vu <tuanmanh@gmail.com>
2019-11-16 00:00:07 +0000

    Added README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+This repo is used with gitpod to set up a working environment for jupyterlab. It does the following
+1. Create a base conda environment (using miniconda)
+2. Provide a script to create a new conda environment. Also install essential packages for running a jupyterlab instance
+
+How to use:
+Once the gitpod completes with building the environment, just run ./envconfig.sh in /workspace/miniconda.
+
--- envconfig.sh
+++ envconfig.sh
@@ -6,36 +6,40 @@ echo Initializing...
 mkdir -p /workspace/conda
 mkdir -p /workspace/data
 
-#Create a new env
+#Create a new env called arcw
 conda create --prefix /workspace/conda/arcw python=3.6 &&
 echo "conda activate /workspace/conda/arcw" > ~/.bashrc &&
 export PATH=/workspace/conda/arcw/bin:$PATH &&
 source ~/.bashrc
 export SHELL=/bin/bash
 
-#Install conda packages
+#Install conda packages for to run jupyterlab
 conda install -y -c conda-forge jupyterlab
 conda install -y -c conda-forge beakerx
-conda install -y -c anaconda pyodbc
 conda install -y -c conda-forge xeus-cling
 conda install -y -c conda-forge xeus-python
-conda install -y -c conda-forge python-graphviz
-conda install -y -c conda-forge tensorflow
-conda install -y -c conda-forge keras
-conda install -y -c conda-forge xtl
-conda install -y -c conda-forge openblas
-conda install -y -c conda-forge gdal
-conda install -y -c conda-forge util-linux
-conda install -y -c conda-forge libtiff
-conda install -y -c conda-forge libgdal
-conda install -y -c pytorch pytorch
-conda install -y -c conda-forge dask
-conda install -y -c conda-forge dash
-conda install -y -c conda-forge dash-table
-conda install -y -c conda-forge rx
-conda install -y -c conda-forge dash-core-components
-conda install -y -c conda-forge cassandra-driver
-conda install -y -c ranaroussi yfinance
+
+# Some extra packages for your environments
+# conda install -y -c anaconda pyodbc
+# conda install -y -c conda-forge xeus-cling
+# conda install -y -c conda-forge xeus-python
+# conda install -y -c conda-forge python-graphviz
+# conda install -y -c conda-forge tensorflow
+# conda install -y -c conda-forge keras
+# conda install -y -c conda-forge xtl
+# conda install -y -c conda-forge openblas
+# conda install -y -c conda-forge gdal
+# conda install -y -c conda-forge util-linux
+# conda install -y -c conda-forge libtiff
+# conda install -y -c conda-forge libgdal
+# conda install -y -c pytorch pytorch
+# conda install -y -c conda-forge dask
+# conda install -y -c conda-forge dash
+# conda install -y -c conda-forge dash-table
+# conda install -y -c conda-forge rx
+# conda install -y -c conda-forge dash-core-components
+# conda install -y -c conda-forge cassandra-driver
+# conda install -y -c ranaroussi yfinance
 
 #Notes
 #To run jupyter lab : jupyter lab --ip=0.0.0.0 --allow-root
